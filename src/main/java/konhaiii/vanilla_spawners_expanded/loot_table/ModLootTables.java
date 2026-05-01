6dddfc712bf151596036a74eb05e0ab6bc84aa46
Konhaiii <iahnok063@gmail.com>
2026-05-01 07:23:17 +0200

    Expanded config file
    Added new config fields to customize loot tables in details.

--- src/main/java/konhaiii/vanilla_spawners_expanded/loot_table/ModLootTables.java
+++ src/main/java/konhaiii/vanilla_spawners_expanded/loot_table/ModLootTables.java
@@ -24,12 +24,15 @@ public class ModLootTables {
 			if (resourceKey == LootTables.SIMPLE_DUNGEON_CHEST) {
 
 				LootPool pool = LootPool.builder()
-						.rolls(ConstantLootNumberProvider.create(1))
-						.conditionally(RandomChanceLootCondition.builder(0.35f))
-						.with(ItemEntry.builder(ModItems.SPAWNER_CALIBRATOR).weight(5))
-						.with(ItemEntry.builder(ModItems.SPAWNER_UPGRADE_CROWD).weight(2))
-						.with(ItemEntry.builder(ModItems.SPAWNER_UPGRADE_RANGE).weight(1))
-						.with(ItemEntry.builder(ModItems.SPAWNER_UPGRADE_REDSTONE).weight(2))
+						.rolls(ConstantLootNumberProvider.create(VanillaSpawnersExpanded.config.lootTableValues.dungeonRolls))
+						.conditionally(RandomChanceLootCondition.builder(VanillaSpawnersExpanded.config.lootTableValues.dungeonChance))
+						.with(ItemEntry.builder(ModItems.SPAWNER_UPGRADE_CROWD).weight(VanillaSpawnersExpanded.config.lootTableValues.dungeonUpgradeCrowdWeight))
+						.with(ItemEntry.builder(ModItems.SPAWNER_UPGRADE_RANGE).weight(VanillaSpawnersExpanded.config.lootTableValues.dungeonUpgradeRangeWeight))
+						.with(ItemEntry.builder(ModItems.SPAWNER_UPGRADE_SPEED).weight(VanillaSpawnersExpanded.config.lootTableValues.dungeonUpgradeSpeedWeight))
+						.with(ItemEntry.builder(ModItems.SPAWNER_UPGRADE_REDSTONE).weight(VanillaSpawnersExpanded.config.lootTableValues.dungeonUpgradeRedstoneWeight))
+						.with(ItemEntry.builder(ModItems.SPAWNER_CALIBRATOR).weight(VanillaSpawnersExpanded.config.lootTableValues.dungeonCalibratorWeight))
+						.with(ItemEntry.builder(ModItems.SPAWNER_IGNITER).weight(VanillaSpawnersExpanded.config.lootTableValues.dungeonIgniterWeight))
+						.with(ItemEntry.builder(ModItems.CURSED_BOTTLE).weight(VanillaSpawnersExpanded.config.lootTableValues.dungeonCursedBottleWeight))
 						.build();
 
 				tableBuilder.pool(pool);
@@ -38,11 +41,15 @@ public class ModLootTables {
 			if (resourceKey == LootTables.END_CITY_TREASURE_CHEST) {
 
 				LootPool pool = LootPool.builder()
-						.rolls(ConstantLootNumberProvider.create(1))
-						.conditionally(RandomChanceLootCondition.builder(0.2f))
-						.with(ItemEntry.builder(ModItems.SPAWNER_CALIBRATOR).weight(1))
-						.with(ItemEntry.builder(ModItems.CURSED_BOTTLE).weight(2))
-						.with(ItemEntry.builder(ModItems.SPAWNER_IGNITER).weight(2))
+						.rolls(ConstantLootNumberProvider.create(VanillaSpawnersExpanded.config.lootTableValues.endCityRolls))
+						.conditionally(RandomChanceLootCondition.builder(VanillaSpawnersExpanded.config.lootTableValues.endCityChance))
+						.with(ItemEntry.builder(ModItems.SPAWNER_UPGRADE_CROWD).weight(VanillaSpawnersExpanded.config.lootTableValues.endCityUpgradeCrowdWeight))
+						.with(ItemEntry.builder(ModItems.SPAWNER_UPGRADE_RANGE).weight(VanillaSpawnersExpanded.config.lootTableValues.endCityUpgradeRangeWeight))
+						.with(ItemEntry.builder(ModItems.SPAWNER_UPGRADE_SPEED).weight(VanillaSpawnersExpanded.config.lootTableValues.endCityUpgradeSpeedWeight))
+						.with(ItemEntry.builder(ModItems.SPAWNER_UPGRADE_REDSTONE).weight(VanillaSpawnersExpanded.config.lootTableValues.endCityUpgradeRedstoneWeight))
+						.with(ItemEntry.builder(ModItems.SPAWNER_CALIBRATOR).weight(VanillaSpawnersExpanded.config.lootTableValues.endCityCalibratorWeight))
+						.with(ItemEntry.builder(ModItems.SPAWNER_IGNITER).weight(VanillaSpawnersExpanded.config.lootTableValues.endCityIgniterWeight))
+						.with(ItemEntry.builder(ModItems.CURSED_BOTTLE).weight(VanillaSpawnersExpanded.config.lootTableValues.endCityCursedBottleWeight))
 						.build();
 
 				tableBuilder.pool(pool);
@@ -51,13 +58,15 @@ public class ModLootTables {
 			if (resourceKey == LootTables.BASTION_TREASURE_CHEST) {
 
 				LootPool pool = LootPool.builder()
-						.rolls(ConstantLootNumberProvider.create(1))
-						.conditionally(RandomChanceLootCondition.builder(0.25f))
-						.with(ItemEntry.builder(ModItems.SPAWNER_CALIBRATOR).weight(1))
-						.with(ItemEntry.builder(ModItems.SPAWNER_UPGRADE_RANGE).weight(3))
-						.with(ItemEntry.builder(ModItems.SPAWNER_UPGRADE_CROWD).weight(3))
-						.with(ItemEntry.builder(ModItems.SPAWNER_UPGRADE_SPEED).weight(1))
-						.with(ItemEntry.builder(ModItems.SPAWNER_IGNITER).weight(2))
+						.rolls(ConstantLootNumberProvider.create(VanillaSpawnersExpanded.config.lootTableValues.bastionRolls))
+						.conditionally(RandomChanceLootCondition.builder(VanillaSpawnersExpanded.config.lootTableValues.bastionChance))
+						.with(ItemEntry.builder(ModItems.SPAWNER_UPGRADE_CROWD).weight(VanillaSpawnersExpanded.config.lootTableValues.bastionUpgradeCrowdWeight))
+						.with(ItemEntry.builder(ModItems.SPAWNER_UPGRADE_RANGE).weight(VanillaSpawnersExpanded.config.lootTableValues.bastionUpgradeRangeWeight))
+						.with(ItemEntry.builder(ModItems.SPAWNER_UPGRADE_SPEED).weight(VanillaSpawnersExpanded.config.lootTableValues.bastionUpgradeSpeedWeight))
+						.with(ItemEntry.builder(ModItems.SPAWNER_UPGRADE_REDSTONE).weight(VanillaSpawnersExpanded.config.lootTableValues.bastionUpgradeRedstoneWeight))
+						.with(ItemEntry.builder(ModItems.SPAWNER_CALIBRATOR).weight(VanillaSpawnersExpanded.config.lootTableValues.bastionCalibratorWeight))
+						.with(ItemEntry.builder(ModItems.SPAWNER_IGNITER).weight(VanillaSpawnersExpanded.config.lootTableValues.bastionIgniterWeight))
+						.with(ItemEntry.builder(ModItems.CURSED_BOTTLE).weight(VanillaSpawnersExpanded.config.lootTableValues.bastionCursedBottleWeight))
 						.build();
 
 				tableBuilder.pool(pool);
@@ -66,12 +75,15 @@ public class ModLootTables {
 			if (resourceKey == LootTables.ANCIENT_CITY_CHEST) {
 
 				LootPool pool = LootPool.builder()
-						.rolls(ConstantLootNumberProvider.create(1))
-						.conditionally(RandomChanceLootCondition.builder(0.3f))
-						.with(ItemEntry.builder(ModItems.SPAWNER_CALIBRATOR).weight(1))
-						.with(ItemEntry.builder(ModItems.SPAWNER_UPGRADE_RANGE).weight(1))
-						.with(ItemEntry.builder(ModItems.SPAWNER_UPGRADE_SPEED).weight(1))
-						.with(ItemEntry.builder(ModItems.SPAWNER_IGNITER).weight(2))
+						.rolls(ConstantLootNumberProvider.create(VanillaSpawnersExpanded.config.lootTableValues.ancientCityRolls))
+						.conditionally(RandomChanceLootCondition.builder(VanillaSpawnersExpanded.config.lootTableValues.ancientCityChance))
+						.with(ItemEntry.builder(ModItems.SPAWNER_UPGRADE_CROWD).weight(VanillaSpawnersExpanded.config.lootTableValues.ancientCityUpgradeCrowdWeight))
+						.with(ItemEntry.builder(ModItems.SPAWNER_UPGRADE_RANGE).weight(VanillaSpawnersExpanded.config.lootTableValues.ancientCityUpgradeRangeWeight))
+						.with(ItemEntry.builder(ModItems.SPAWNER_UPGRADE_SPEED).weight(VanillaSpawnersExpanded.config.lootTableValues.ancientCityUpgradeSpeedWeight))
+						.with(ItemEntry.builder(ModItems.SPAWNER_UPGRADE_REDSTONE).weight(VanillaSpawnersExpanded.config.lootTableValues.ancientCityUpgradeRedstoneWeight))
+						.with(ItemEntry.builder(ModItems.SPAWNER_CALIBRATOR).weight(VanillaSpawnersExpanded.config.lootTableValues.ancientCityCalibratorWeight))
+						.with(ItemEntry.builder(ModItems.SPAWNER_IGNITER).weight(VanillaSpawnersExpanded.config.lootTableValues.ancientCityIgniterWeight))
+						.with(ItemEntry.builder(ModItems.CURSED_BOTTLE).weight(VanillaSpawnersExpanded.config.lootTableValues.ancientCityCursedBottleWeight))
 						.build();
 
 				tableBuilder.pool(pool);
